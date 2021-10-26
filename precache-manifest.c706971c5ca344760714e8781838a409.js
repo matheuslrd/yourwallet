60575e6581ceb0cdb5c110c7f8711a26b015349d
Matheus Laurindo <matheus104521@gmail.com>
2021-10-26 19:46:46 -0300

    Updates

---
+++ precache-manifest.c706971c5ca344760714e8781838a409.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d0cc20f272e9ef43d0dcf24762df20b",
+    "url": "/yourwallet/index.html"
+  },
+  {
+    "revision": "7ed06364101e75dfd843",
+    "url": "/yourwallet/static/css/main.a52f5e57.chunk.css"
+  },
+  {
+    "revision": "a2abef468989f65b85ef",
+    "url": "/yourwallet/static/js/2.bc22b278.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/yourwallet/static/js/2.bc22b278.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ed06364101e75dfd843",
+    "url": "/yourwallet/static/js/main.7f87f3b6.chunk.js"
+  },
+  {
+    "revision": "3e316bd83bf58939aba8",
+    "url": "/yourwallet/static/js/runtime-main.2f48988a.js"
+  }
+]);
